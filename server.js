67620a7cc1ff510359efc960dfaaafa673488591
mayu <savagesuga257@gmail.com>
2026-07-14 00:29:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mysql = require("mysql2");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-app.use(cors());
+
 require("dotenv").config();
 const path = require("path");
 
